ed2afbe7323b581a4f087688ac2dd614d47e90bc
ADARSH R <adarshrajpkd@gmail.com>
2023-05-04 04:49:59 +0530

    thurd

--- Python/Python Code/movement.py
+++ Python/Python Code/movement.py
@@ -1,15 +1,20 @@
 #! /usr/bin/env python
 
+#import
 import pybullet as p
 import numpy as np
 import time
+import matplotlib.pyplot as plt
 import pybullet_data as pd
 import rospy
 import sys
 from std_msgs.msg import Float64
 from std_srvs.srv import Empty
+from trajectory_msgs.msg import JointTrajectory, JointTrajectoryPoint
 from open_manipulator_msgs.srv import SetJointPosition, SetJointPositionRequest
+from sensor_msgs.msg import JointState
 
+#initialise
 p.connect(p.GUI)
 p.setGravity(0, 0, -9.81)
 p.setTimeStep(1./240.)
@@ -18,7 +23,27 @@ robot = p.loadURDF("/home/adarsh/catkin_ws/src/open_manipulator/open_manipulator
 num_joints = p.getNumJoints(robot)
 joint_indices = list(range(num_joints))
 joint_damping = [0.1] * num_joints
+joint_angles = {joint_name: [] for joint_name in ['joint1', 'joint2', 'joint3', 'joint4', 'gripper']}
 
+
+#functions
+def callback(data):
+    # Store the joint angles for each joint
+    for i in range(len(data.name)):
+        joint_name = data.name[i]
+        joint_angle = data.position[i]
+        joint_angles[joint_name].append(joint_angle)
+def plot_fk():
+     rospy.Subscriber('/open_manipulator_x/joint_states', JointState, callback)
+     while not rospy.is_shutdown():
+          plt.clf()
+          for joint_name in joint_angles.keys():
+               plt.plot(joint_angles[joint_name])
+          plt.xlabel('Time (s)')
+          plt.ylabel('Joint Angle (rad)')
+          plt.title('Joint Angles of Open Manipulator X')
+          plt.legend(joint_angles.keys())
+          plt.pause(0.1)         
 def ik_move(a):
     x = a[0]
     y = a[1]
@@ -30,7 +55,6 @@ def ik_move(a):
     end_effector_pose[:3, 3] = end_effector_pos
     joint_angles = p.calculateInverseKinematics(robot, num_joints - 1, end_effector_pos, end_effector_orient.flatten(), jointDamping=joint_damping)
     fk_move([joint_angles[0],joint_angles[1],joint_angles[2],joint_angles[3]])
-
 def fk_move(a):
     rospy.init_node('service_set_joint_position_client')
     rospy.wait_for_service('/goal_joint_space_path')
@@ -41,12 +65,11 @@ def fk_move(a):
     goal_joint_space_path_request_object.joint_position.position = a
     goal_joint_space_path_request_object.joint_position.max_accelerations_scaling_factor = 1.0
     goal_joint_space_path_request_object.joint_position.max_velocity_scaling_factor = 1.0
-    goal_joint_space_path_request_object.path_time = 5.0
+    goal_joint_space_path_request_object.path_time = 4
     rospy.loginfo("Doing Service Call")
     result = goal_joint_space_path_service_client(goal_joint_space_path_request_object)
     print (result)
     rospy.sleep(5)
-
 def grip_move(x):
     if (x== 'open'):
         x = [0.01]
@@ -66,10 +89,6 @@ def grip_move(x):
     result = goal_joint_space_path_service_client(goal_joint_space_path_request_object)
     print (result)
     rospy.sleep(2)
-
-home = [0,0,0,0]
-inter = [0,-1.05,0.35,0.7]
-
 def ansi_color(n,s):
 	code={"bold":1,"faint":2,"italic":3,"underline":4,"blink_slow":5,"blink_fast":6,"negative":7,"conceal":8,"strike_th":9,
 	"black":30,"red":31,"green":32,"yellow":33,"blue":34,"magenda":35,"cyan":36,"white":37,
@@ -80,7 +99,35 @@ def ansi_color(n,s):
 		return value
 	except:
 		pass
-		
+def rect(a):
+     a1 = [0.286,0,0.205]
+     a2 = [0.286,a[0]/2,0.205]
+     a3 = [0.286+a[1],a[0]/2,0.205]
+     a4 = [0.286+a[1],-a[0]/2,0.205]
+     a5 = [0.286,-a[0]/2,0.205]
+     ik_move(a1)
+     ik_move(a2)
+     ik_move(a3)
+     ik_move(a4)
+     ik_move(a5)
+     ik_move(a1)
+def circle(r):
+    radius = r
+    center = [0.286, 0.0, 0.204]
+    num_points = 20
+    speed = 1
+    for i in range(num_points):
+        theta = 2 * math.pi * i / num_points
+        x = center[0] + radius * math.cos(theta)
+        y = center[1] + radius * math.sin(theta)
+        z = center[2]
+        ik_move([x,y,z])
+        rospy.sleep(1 / speed)
+
+home = [0,0,0,0]
+inter = [0,-1.05,0.35,0.7]
+
+
 #grip_move('open')
 #fk_move(home)
 #fk_move(source)
@@ -96,14 +143,16 @@ def ansi_color(n,s):
 #ik_move([0.134,0,0.241])
 #ik_move([0.213,-0.062,0.118])
 
+
 print("\n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n ")
 time.sleep(4)
-print("Pick and Place - Kinematics \n ")
+print("Open Manipulator Kinematics \n ")
 time.sleep(2)
 print ("Enter your selection : \n")
 time.sleep(2)
-print("1. Forward Kinematics \n2. Inverse Kinematics")
-temp = int(input("Enter your selection : "))
+print("1. Forward Kinematics \n2. Inverse Kinematics \n3. Trajectory \n")
+temp = int(input("Your Input : "))
+
 if (temp == 1):
     print("You have selected Forward Kinematics...")
     time.sleep(1.5)
@@ -153,6 +202,23 @@ elif (temp == 2):
     grip_move('open')
     fk_move(home)
     grip_move('close')
-
+elif (temp ==3):
+     print("You have selected Trajectory Planning...")
+     time.sleep(3)
+     print("Choose an option.   \n1.Rectangle \n2.Circle")
+     y = int(input("Enter your input : "))
+     if y == 1:
+          l = list(map(float,
+                 input("Enter the length and breadth : ").strip().split()))[:2]
+          time.sleep(1)
+          print (ansi_color('blink_fast',"Planning"))
+          time.sleep(3)
+          rect(l)
+     elif y == 2:
+          r = float(input("Enter the radius : "))
+          time.sleep(1.5)
+          print (ansi_color('blink_fast',"Planning"))
+          time.sleep(2)
+          circle(r)
 else:
     print("Wrong Input")
